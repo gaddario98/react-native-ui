55c653b4f7aa74e46bdec88050ec496c72a97d28
gaddario98 <gaddario98@gmail.com>
2025-10-17 00:34:07 +0200

    Fix: update package

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ const pkg = require("./package.json");
 // Definizione degli entry points
 const entries = [
   { name: "index", input: "index.ts" },
-  { name: "index", input: "index.ts" },
+    { name: "lib", input: "lib/index.ts" },
   { name: "styles", input: "styles/index.ts" },
 ];
 
--- rollup.dts.config.js
+++ rollup.dts.config.js
@@ -3,7 +3,7 @@ import { createTypeDeclarations } from "../../rollup.common.config.js";
 // Definizione degli entry points (deve essere sincronizzata con rollup.config.js)
 const entries = [
   { name: "index", input: "index.ts" },
-  { name: "index", input: "index.ts" },
+    { name: "lib", input: "lib/index.ts" },
   { name: "styles", input: "styles/index.ts" },
 ];
 
