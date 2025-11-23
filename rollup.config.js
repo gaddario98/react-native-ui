83e779329370fe976d98ca5db10e64727c364b3f
gaddario98 <gaddario98@gmail.com>
2025-11-23 13:10:52 +0100

    chore: update package version to 1.0.29 and include styles in exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import { createRequire } from "module";
-import { 
-  createMultiEntryConfig, 
-  createTypeDeclarations 
+import {
+  createMultiEntryConfig,
+  createTypeDeclarations,
 } from "../../rollup.common.config.js";
 
 const require = createRequire(import.meta.url);
@@ -10,11 +10,11 @@ const pkg = require("./package.json");
 // Definizione degli entry points
 const entries = [
   { name: "index", input: "index.ts" },
-    { name: "lib", input: "lib/index.ts" },
+  { name: "lib", input: "lib/index.ts" },
   { name: "styles", input: "styles/index.ts" },
 ];
 
 // Configurazione per i file JavaScript
-export default createMultiEntryConfig(pkg, entries, { 
+export default createMultiEntryConfig(pkg, entries, {
   isReactNative: true,
 });
